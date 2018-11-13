b2026a173980c2058556a9a637c20894cf7c39d5
tingting <836682424@qq.com>
2018-11-13 15:53:31 +0800

    sticky navbar

--- T24/resume-4/main.js
+++ T24/resume-4/main.js
@@ -2,3 +2,12 @@
 setTimeout(function(){
     siteWelcome.classList.remove("active")
 },1000)
+
+//鼠标中轮滚动时间
+window.onscroll = function(){
+    if(window.scrollY>0){
+        topNavbar.classList.add('scrollStyle');
+    }else{
+        topNavbar.classList.remove('scrollStyle');
+    }
+}
