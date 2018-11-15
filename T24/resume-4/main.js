91e35a5f1fa19cf8c00971a0d863f8b2e026913c
tingting <836682424@qq.com>
2018-11-15 14:09:13 +0800

    点击导航栏自己跳转到相应div

--- T24/resume-4/main.js
+++ T24/resume-4/main.js
@@ -13,12 +13,24 @@ window.onscroll = function(){
 }
 
 //导航下划线从左往右划动
-liTags = document.querySelectorAll("nav.menu>ul>li");
+var liTags = document.querySelectorAll("nav.menu>ul>li");
 for(let i = 0; i<liTags.length;i++){
     liTags[i].onmouseenter=function(x){
         x.currentTarget.classList.add("active");
     }
     liTags[i].onmouseleave=function(x){
         x.currentTarget.classList.remove("active");
+    }
+}
+//点击导航栏按钮，页面跳至相应位置
+var aTags = document.querySelectorAll("nav.menu>ul>li>a");
+for (let i=0;i<aTags.length;i++){
+    aTags[i].onclick = function(x){
+        x.preventDefault(); //阻止a标签的默认跳转功能
+        let a = x.currentTarget;
+        let href = a.getAttribute("href");
+        let element = document.querySelector(href);
+        let top = element.offsetTop;
+        window.scrollTo(0,top-80);
     }
 }
