21c1d9ae7f954d8eea6adb719bfed09e20be751f
tingting <836682424@qq.com>
2018-11-22 17:17:04 +0800

    下沉效果+技能条效果

--- T25/resume-5/main.js
+++ T25/resume-5/main.js
@@ -1,16 +1,46 @@
 //强行显示loading
 setTimeout(function(){
     siteWelcome.classList.remove("active")
-},1000)
+},1000);
+
+//元素自动浮起
+let specialTags = document.querySelectorAll('[data-x]')
+for(let i =0;i<specialTags.length; i++){
+  specialTags[i].classList.add('offset')
+}
 
 //鼠标中轮滚动时间
+findCloset()
 window.onscroll = function(){
     if(window.scrollY>0){
         topNavbar.classList.add('scrollStyle');
     }else{
         topNavbar.classList.remove('scrollStyle');
     }
+    findCloset()
 }
+
+function findCloset(){
+    let specialTags = document.querySelectorAll('[data-x]');
+    let minIndex = 0;
+    for (let i=0;i<specialTags.length;i++){
+        if(Math.abs(specialTags[i].offsetTop-window.scrollY)<Math.abs(specialTags[minIndex].offsetTop-window.scrollY)){
+           minIndex = i;
+        }
+    }
+    // minIndex 就是里窗口顶部最近的元素
+   specialTags[minIndex].classList.remove('offset')
+    let id = specialTags[minIndex].id;
+    let a = document.querySelector('a[href="#'+ id + '"]');
+    let li = a.parentNode
+    let brothersAndMe = li.parentNode.children
+    for(let i=0; i<brothersAndMe.length; i++){
+       brothersAndMe[i].classList.remove('highlight')
+    }
+    li.classList.add('highlight')
+
+}
+
 
 //导航下划线从左往右划动
 var liTags = document.querySelectorAll("nav.menu>ul>li");
