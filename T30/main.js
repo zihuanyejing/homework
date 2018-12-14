b890c2a86d2371c3fd9f5bfca8af5cfa57f833b5
tingting <836682424@qq.com>
2018-12-14 14:28:59 +0800

    自动轮播

--- T30/main.js
+++ T30/main.js
@@ -1,4 +1,6 @@
 let allButtons=$("#buttons>svg");
+let n = 0;
+let size = allButtons.length;
 for (let i =0; i<allButtons.length;i++){
     $(allButtons[i]).on('click',function(x){
         let index = $(x.currentTarget).index();
@@ -8,7 +10,19 @@ for (let i =0; i<allButtons.length;i++){
         });
         n =index;
         allButtons.eq(n).addClass('selected').siblings('.selected').removeClass('selected');
-
-
     });
 }
+allButtons.eq(n%size).trigger('click').addClass('selected').siblings('.selected').removeClass('selected');
+let timeId = setInterval(function(){
+    n=n+1;
+    allButtons.eq(n%size).trigger('click').addClass('selected').siblings('.selected').removeClass('selected');
+   },1000);
+$(".window").on("mouseenter",function(){
+    window.clearInterval(timeId);
+});
+$(".window").on("mouseleave",function(){
+    timeId = setInterval(function(){
+        n=n+1;
+        allButtons.eq(n%size).trigger('click').addClass('selected').siblings('.selected').removeClass('selected');
+       },1000);
+});
